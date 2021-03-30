f680790209fb006a07d934247024a9c33eac5118
DESKTOP-CB0EGJG\Nadya <nr@redtower.by>
2021-03-30 10:09:29 +0300

    Update Game2

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -6,11 +6,16 @@ using UnityEngine.UI;
 public class NewBehaviourScript : MonoBehaviour
 {
 	  private int result, turnsNumber, gameOverValue = 50;
-            
+      public Text text, textEnd, textStart;
+      
     // Start is called before the first frame update
     void Start()
     {
-        StartGame();
+        result = 0;
+        turnsNumber = 0;
+        print("Введите число от 0 до 9.");
+        textStart.text = "Введите число от 0 до 9.";
+
     }
 
     // Update is called once per frame
@@ -19,22 +24,14 @@ public class NewBehaviourScript : MonoBehaviour
         CheckPressedKeys();
     }
 
-    private void StartGame() 
-    {
-        result = 0;
-        turnsNumber = 0;
-        ShowTurnMessage();
-    }
+  
 
-    private void ShowTurnMessage() 
-    {
-        print("Результат - " + result + ".");
-        print("Введите число от 0 до 9.");
-    }
+  
 
     private void ShowGameOverMessage() 
     {
         print("Игра окончена. Результат - " + result + ". Количество ходов - " + turnsNumber + ".");
+        textEnd.text = "Игра окончена. Результат - " + result + ". Количество ходов - " + turnsNumber + ".";
     }
 
     private void CheckPressedKeys()
@@ -44,49 +41,59 @@ public class NewBehaviourScript : MonoBehaviour
         if (Input.GetKeyDown (KeyCode.Keypad1) || Input.GetKeyDown (KeyCode.Alpha1))
         {
             keyValue = 1;
+            result = keyValue + result;
         }
         
-        else if (Input.GetKeyDown (KeyCode.Keypad2) || Input.GetKeyDown (KeyCode.Alpha2))
+        if (Input.GetKeyDown (KeyCode.Keypad2) || Input.GetKeyDown (KeyCode.Alpha2))
         {
             keyValue = 2;
+            result = keyValue + result;
         }
-        else if (Input.GetKeyDown (KeyCode.Keypad3) || Input.GetKeyDown (KeyCode.Alpha3))
+        if (Input.GetKeyDown (KeyCode.Keypad3) || Input.GetKeyDown (KeyCode.Alpha3))
         {
             keyValue = 3;
+            result = keyValue + result;
         }
-        else if (Input.GetKeyDown (KeyCode.Keypad4) || Input.GetKeyDown (KeyCode.Alpha4))
+        if (Input.GetKeyDown (KeyCode.Keypad4) || Input.GetKeyDown (KeyCode.Alpha4))
         {
-            keyValue = 4;
+            keyValue = 4; 
+            result = keyValue + result;
         }
-        else if (Input.GetKeyDown (KeyCode.Keypad5) || Input.GetKeyDown (KeyCode.Alpha5))
+        if (Input.GetKeyDown (KeyCode.Keypad5) || Input.GetKeyDown (KeyCode.Alpha5))
         {
             keyValue = 5;
+            result = keyValue + result;
         }
-        else if (Input.GetKeyDown (KeyCode.Keypad6) || Input.GetKeyDown (KeyCode.Alpha6))
+        if (Input.GetKeyDown (KeyCode.Keypad6) || Input.GetKeyDown (KeyCode.Alpha6))
         {
             keyValue = 6;
+            result = keyValue + result;
         }
-        else if (Input.GetKeyDown (KeyCode.Keypad7) || Input.GetKeyDown (KeyCode.Alpha7))
+        if (Input.GetKeyDown (KeyCode.Keypad7) || Input.GetKeyDown (KeyCode.Alpha7))
         {
             keyValue = 7;
+            result = keyValue + result;
         }
-        else if (Input.GetKeyDown (KeyCode.Keypad8) || Input.GetKeyDown (KeyCode.Alpha8))
+        if (Input.GetKeyDown (KeyCode.Keypad8) || Input.GetKeyDown (KeyCode.Alpha8))
         {
             keyValue = 8;
+            result = keyValue + result;
         }
-        else if (Input.GetKeyDown (KeyCode.Keypad9) || Input.GetKeyDown (KeyCode.Alpha9))
+        if (Input.GetKeyDown (KeyCode.Keypad9) || Input.GetKeyDown (KeyCode.Alpha9))
         {
             keyValue = 9;
+            result = keyValue + result;
         }
-        else if (Input.GetKeyDown (KeyCode.Space))
+        if (Input.GetKeyDown (KeyCode.Space))
         {
-            StartGame();
+             result = 0;
+             turnsNumber = 0;
+             print("Введите число от 0 до 9.");
         }
 
 
         if (keyValue > 0)
         {
-            result += keyValue;
             turnsNumber++;
             CheckGameState();
         }
@@ -97,11 +104,16 @@ public class NewBehaviourScript : MonoBehaviour
         if (result >= gameOverValue)
         {
             ShowGameOverMessage();
-            StartGame();
+           
         }
         else
         {
-            ShowTurnMessage();
+            print("Результат - " + result + ".");
+            text.text = "Результат - " + result + ".";
+            print("Введите число от 0 до 9.");
+            textStart.text = "Введите число от 0 до 9.";
+                       
+            
         }
     }
 }
